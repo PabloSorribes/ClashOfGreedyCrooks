fbe1c31274771c15e6f80b95c3dda206a2180b5e
Jim Andersson <andersson.jim@outlook.com>
2018-01-26 15:10:47 +0100

    Added pause/unpause function

--- ClashOfGreedyCrooks/Assets/_Scripts/GameStateManager.cs
+++ ClashOfGreedyCrooks/Assets/_Scripts/GameStateManager.cs
@@ -12,6 +12,9 @@ public class GameStateManager : MonoBehaviour {
     public enum State { MainMenu, PlayerConnect, Picking, Arena };
     private State state;
 
+    public enum PauseState { Paused, NotPaused };
+    private PauseState pauseState;
+
     private void Awake()
     {
         instance = this;
@@ -31,6 +34,10 @@ public class GameStateManager : MonoBehaviour {
         }
     }
 
+    /// <summary>
+    /// Changes the scene
+    /// </summary>
+    /// <param name="newState"></param>
     public void SetState(State newState)
     {
         if (newState == State.MainMenu)
@@ -64,5 +71,29 @@ public class GameStateManager : MonoBehaviour {
     private void OnArenaState()
     {
         SceneManager.LoadScene("Arena01");
+    }
+
+    /// <summary>
+    /// Pause or unpause the game
+    /// </summary>
+    /// <param name="newState"></param>
+    public void SetPausedState(PauseState newState)
+    {
+        if (newState == PauseState.Paused)
+            OnPausedState();
+        else if (newState == PauseState.NotPaused)
+            OnNotPausedState();
+
+        pauseState = newState;
+    }
+
+    private void OnPausedState()
+    {
+
+    }
+
+    private void OnNotPausedState()
+    {
+
     }
 }
