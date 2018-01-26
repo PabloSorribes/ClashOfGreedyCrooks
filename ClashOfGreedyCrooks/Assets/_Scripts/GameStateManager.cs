0d175364a6da8e7cf3fed411fad7b8a26b441b9d
Jim Andersson <andersson.jim@outlook.com>
2018-01-26 13:28:02 +0100

    Added GameStateManager script

---
+++ ClashOfGreedyCrooks/Assets/_Scripts/GameStateManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStateManager : MonoBehaviour {
+
+	public enum State { MainMenu, PlayerConnect, Picking, Arena };
+    private State state;
+
+    public void SetState(State newState)
+    {
+        if (newState == State.MainMenu)
+            OnMainMenuState();
+        else if (newState == State.PlayerConnect)
+            OnPlayerConnectState();
+        else if (newState == State.Picking)
+            OnPickingState();
+        else if (newState == State.Arena)
+            OnArenaState();
+
+        state = newState;
+    }
+
+    private void OnMainMenuState()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    private void OnPlayerConnectState()
+    {
+        SceneManager.LoadScene("PlayerConnect");
+    }
+
+    private void OnPickingState()
+    {
+        SceneManager.LoadScene("Picking");
+    }
+
+    private void OnArenaState()
+    {
+        SceneManager.LoadScene("Arena01");
+    }
+}
