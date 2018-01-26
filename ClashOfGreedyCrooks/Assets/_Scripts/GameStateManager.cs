e83c40d3a4c8d8c28ca319e2aac7c79260e0c91a
Jim Andersson <andersson.jim@outlook.com>
2018-01-26 14:02:24 +0100

    Added scripts: GameManager, PlayerController, PlayerConnectManager

--- ClashOfGreedyCrooks/Assets/_Scripts/GameStateManager.cs
+++ ClashOfGreedyCrooks/Assets/_Scripts/GameStateManager.cs
@@ -3,8 +3,33 @@ using UnityEngine.SceneManagement;
 
 public class GameStateManager : MonoBehaviour {
 
-	public enum State { MainMenu, PlayerConnect, Picking, Arena };
+    private static GameStateManager instance;
+    public static GameStateManager GetInstance()
+    {
+        return instance;
+    }
+
+    public enum State { MainMenu, PlayerConnect, Picking, Arena };
     private State state;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (FindObjectOfType<GameStateManager>().gameObject != this.gameObject)
+        {
+            Destroy(FindObjectOfType<GameStateManager>().gameObject);
+        }
+    }
 
     public void SetState(State newState)
     {
@@ -22,6 +47,7 @@ public class GameStateManager : MonoBehaviour {
 
     private void OnMainMenuState()
     {
+        GameManager.GetInstance().ResetGame();
         SceneManager.LoadScene("MainMenu");
     }
 
