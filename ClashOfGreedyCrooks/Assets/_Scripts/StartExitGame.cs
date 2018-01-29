24fbb5f044bc08fbfa4de0ff413edd51a347db9e
FilipJinstrand <filip@jinstrand.se>
2018-01-29 15:57:04 +0100

    Fixade menyn, la till Start samt exit knapp. La till Test_Filipj scene

---
+++ ClashOfGreedyCrooks/Assets/_Scripts/StartExitGame.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartExitGame : MonoBehaviour {
+
+    public Button startButton;
+    public Button exitButton;
+
+    // Use this for initialization
+    void Start () {
+        startButton.onClick.AddListener(StartButtonClicked);
+        exitButton.onClick.AddListener(ExitButtonClicked);
+	}
+	
+	// Update is called once per frame
+	void StartButtonClicked()
+    {
+        SceneManager.LoadScene("PlayerConnect");
+    }
+
+    void ExitButtonClicked ()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+	Application.OpenURL(webplayerQuitURL);
+#else
+	Application.Quit();
+#endif
+    }
+}
