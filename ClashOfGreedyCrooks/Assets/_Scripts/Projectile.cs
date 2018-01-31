c8f6e7fa6db7cd78617341bb7a156ab124eab5ee
Zimba1 <31646893+Zimba1@users.noreply.github.com>
2018-01-31 10:43:13 +0100

    Projectile script

---
+++ ClashOfGreedyCrooks/Assets/_Scripts/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public int speed;
+    private int dmg;
+    GameObject player;
+
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag=="Player")
+        {
+            player = collision.gameObject;
+            //player.GetComponent<Player>().health-=dmg;
+        }
+        else
+        {
+
+        }
+    }
+}
