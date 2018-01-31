fd4766b89350bf85e060e5004aadd1a4b3b5a347
FilipJinstrand <filip@jinstrand.se>
2018-01-31 17:02:04 +0100

    Added TimeManager and added a timer in my test scene

---
+++ ClashOfGreedyCrooks/Assets/_Scripts/TimeManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour {
+
+    private float trackTime = 120;
+
+    private bool isPaused;
+
+    public GameObject player;
+
+    public Text timer;
+
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        trackTime -= Time.deltaTime;
+        timer.text = "Timer: " + Mathf.Floor(trackTime);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }   
+    }
+
+    private void PauseGame()
+    {
+
+        if (!isPaused)
+        {
+            Time.timeScale = 0;
+
+            isPaused = true;
+
+            player.GetComponent<Shooting>().enabled = false;
+        }
+
+        else
+        {
+            Time.timeScale = 1;
+
+            isPaused = false;
+
+            player.GetComponent<Shooting>().enabled = true;
+        }
+
+    }
+}
