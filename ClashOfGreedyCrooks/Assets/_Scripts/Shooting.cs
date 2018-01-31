6c9a11acb0b9289dd26aa032a172d4d0f87a112e
FilipJinstrand <filip@jinstrand.se>
2018-01-31 09:48:43 +0100

    Added stuff to my test scene, made the shooting script

---
+++ ClashOfGreedyCrooks/Assets/_Scripts/Shooting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+    public GameObject bullet;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            bullet = Instantiate(bullet, transform.position, Quaternion.identity) as GameObject;
+            bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 10);
+        }
+	}
+}
