0c56a158294ee718b73c3064e835d81b3e6770cf
Filip Nilsson <filip_nilsson28@hotmail.com>
2018-02-01 15:27:44 +0100

    Updated InputManager and simple version of PlayerController

--- ClashOfGreedyCrooks/Assets/_Scripts/InputManager.cs
+++ ClashOfGreedyCrooks/Assets/_Scripts/InputManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using XInputDotNetPure;
+using UnityEngine.SceneManagement;
 
 public class InputManager : MonoBehaviour
 {
@@ -9,7 +10,7 @@ public class InputManager : MonoBehaviour
 
 	private State gameState;
 
-	private PlayerIndex[] gamepadIndex = new PlayerIndex[4];
+	private List<PlayerIndex> gamepadIndex = new List<PlayerIndex>();
 	private GamePadState[] state = new GamePadState[4];
 	private GamePadState[] prevState = new GamePadState[4];
 
@@ -20,10 +21,18 @@ public class InputManager : MonoBehaviour
 		instance = this;
 	}
 
+	private void OnEnable()
+	{
+		SceneManager.sceneLoaded += OnSceneChanged;
+	}
+
 	private void Start()
 	{
 		gameState = GameStateManager.GetInstance().GetState();
-		//players[0]
+
+		//For testing
+		//gameState = State.Arena;
+		//players[0] = FindObjectOfType<PlayerController>();
 
 		AddConnectedGamepads();
 	}
@@ -44,7 +53,7 @@ public class InputManager : MonoBehaviour
 			if (testState.IsConnected)
 			{
 				Debug.Log(string.Format("GamePad found {0}", testGamepadIndex));
-				gamepadIndex[i] = testGamepadIndex;
+				gamepadIndex.Add(testGamepadIndex);
 			}
 		}
 	}
@@ -57,7 +66,7 @@ public class InputManager : MonoBehaviour
 	void Update()
 	{
 		//Loop through inputs for every connected gamepad
-		for (int i = 0; i < gamepadIndex.Length; ++i)
+		for (int i = 0; i < gamepadIndex.Count; ++i)
 		{
 			prevState[i] = state[i];
 			state[i] = GamePad.GetState((PlayerIndex)i);
@@ -66,88 +75,151 @@ public class InputManager : MonoBehaviour
 			switch ((int)gameState)
 			{
 				case 0: //Main Menu
-						//TODO: Poll through inputs on all controllers and call relevant method
+					
+					//TODO: Poll through inputs on all controllers and call relevant method
 
 					break;
 
-				case 1: //Player Connect
+				case  1: //Player Connect
 
 					// Detect if a button was pressed this frame
 					if (prevState[i].Buttons.A == ButtonState.Released &&
-						state[i].Buttons.A == ButtonState.Pressed)
+							state[i].Buttons.A == ButtonState.Pressed)
 					{
 						PlayerConnectManager.GetInstance().AddPlayer((int)gamepadIndex[i]);
 						Debug.Log("Player " + i + " pressed button A on Gamepad " + i);
 					}
 
 					if (prevState[i].Buttons.B == ButtonState.Released &&
-						state[i].Buttons.B == ButtonState.Pressed)
+							state[i].Buttons.B == ButtonState.Pressed)
 					{
 						PlayerConnectManager.GetInstance().RemovePlayer((int)gamepadIndex[i]);
 
 						Debug.Log("Player " + i + " pressed button B on Gamepad " + i);
 					}
+					if (prevState[i].Buttons.Start == ButtonState.Released &&
+							state[i].Buttons.Start == ButtonState.Pressed)
+					{
+						//TODO: Call function to start game when all players are ready
+					}
 
 					break;
 
 				case 2: //Picking
-						//TODO: Poll through inputs on all controllers and call relevant method
 
+					if (prevState[i].Buttons.A == ButtonState.Released &&
+							state[i].Buttons.A == ButtonState.Pressed)
+					{
+						PickingManager.GetInstance().PickChampion(i, 0);
+						Debug.Log("Player " + i + " pressed button A on Gamepad " + i);
+					}
+
+					if (prevState[i].Buttons.B == ButtonState.Released &&
+							state[i].Buttons.B == ButtonState.Pressed)
+					{
+						PickingManager.GetInstance().PickChampion(i, 1);
+
+						Debug.Log("Player " + i + " pressed button B on Gamepad " + i);
+					}
+
+					if (prevState[i].Buttons.X == ButtonState.Released &&
+							state[i].Buttons.X == ButtonState.Pressed)
+					{
+						PickingManager.GetInstance().PickChampion(i, 2);
+
+						Debug.Log("Player " + i + " pressed button X on Gamepad " + i);
+					}
+
+					if (prevState[i].Buttons.Y == ButtonState.Released &&
+							state[i].Buttons.Y == ButtonState.Pressed)
+					{
+						PickingManager.GetInstance().PickChampion(i, 3);
+
+						Debug.Log("Player " + i + " pressed button Y on Gamepad " + i);
+					}
 
 					break;
 
 				case 3: //Arena
-						//TODO: Poll through inputs on all controllers and call relevant method
-					if (prevState[i].Buttons.A == ButtonState.Released &&
-					state[i].Buttons.A == ButtonState.Pressed)
+
+					Vector3 leftStick = new Vector3(state[i].ThumbSticks.Left.X, 0f, state[i].ThumbSticks.Left.Y);
+					Vector3 rightStick = new Vector3(state[i].ThumbSticks.Right.X, 0f, state[i].ThumbSticks.Right.Y);
+					//Send directional input data to each player
+					players[i].SetDirectionalInput(leftStick, rightStick);
+
+					if (state[i].Triggers.Left > 0)
+					{
+						//TODO: Alt. Fire or Skill
+					}
+
+					if (state[i].Triggers.Right > 0)
 					{
 						//players[i].Shoot();
-						Debug.Log("Player " + i + " pressed button A on Gamepad " + i);
+					}
+
+					if (prevState[i].Buttons.LeftShoulder == ButtonState.Released &&
+							state[i].Buttons.LeftShoulder == ButtonState.Pressed)
+					{
+						//TODO: Alt. Fire or Skill
+					}
+
+					if (prevState[i].Buttons.RightShoulder == ButtonState.Released &&
+							state[i].Buttons.RightShoulder == ButtonState.Pressed)
+					{
+						//players[i].Shoot();
 					}
 
 					break;
 			}
-
-
-			if (prevState[i].Buttons.A == ButtonState.Released &&
-					state[i].Buttons.A == ButtonState.Pressed)
-			{
-				Debug.Log("Player " + i + " pressed button A on Gamepad " + i);
-			}
-
-			Vector3 leftStick = new Vector3(state[i].ThumbSticks.Left.X, 0f, state[i].ThumbSticks.Left.Y);
-			Vector3 rightStick = new Vector3(state[i].ThumbSticks.Right.X, 0f, state[i].ThumbSticks.Right.Y);
-
-			//players[i].SetDirectionalInput(leftStick, rightStick);
-
 		}
 	}
 
-
-	void FixedUpdate()
+	private void OnSceneChanged(Scene newScene, LoadSceneMode loadingMode)
 	{
-		for (int i = 0; i < gamepadIndex.Length; ++i)
+		if (newScene.name == "MainMenu")
 		{
-			//SetVibration should be sent in a slower rate.
-			//Set vibration according to triggers
-			GamePad.SetVibration((PlayerIndex)i, state[i].Triggers.Left, state[i].Triggers.Right);
+			gameState = State.MainMenu;
 		}
+		if (newScene.name == "PlayerConnect")
+		{
+			gameState = State.PlayerConnect;
+		}
+		if (newScene.name == "Picking")
+		{
+			gameState = State.Picking;
+		}
+		if (newScene.name == "Arena01")
+		{
+			gameState = State.Arena;
+		}
+		Debug.Log(gameState.ToString());
 	}
 
-	void OnGUI()
-	{
-		for (int i = 0; i < gamepadIndex.Length; ++i)
-		{
-			string text = string.Format("Player {0} \n", i);
-		text += string.Format("IsConnected {0} Packet #{1}\n", state[i].IsConnected, state[i].PacketNumber);
-		text += string.Format("\tTriggers {0} {1}\n", state[i].Triggers.Left, state[i].Triggers.Right);
-		text += string.Format("\tD-Pad {0} {1} {2} {3}\n", state[i].DPad.Up, state[i].DPad.Right, state[i].DPad.Down, state[i].DPad.Left);
-		text += string.Format("\tButtons Start {0} Back {1} Guide {2}\n", state[i].Buttons.Start, state[i].Buttons.Back, state[i].Buttons.Guide);
-		text += string.Format("\tButtons LeftStick {0} RightStick {1} LeftShoulder {2} RightShoulder {3}\n", state[i].Buttons.LeftStick, state[i].Buttons.RightStick, state[i].Buttons.LeftShoulder, state[i].Buttons.RightShoulder);
-		text += string.Format("\tButtons A {0} B {1} X {2} Y {3}\n", state[i].Buttons.A, state[i].Buttons.B, state[i].Buttons.X, state[i].Buttons.Y);
-		text += string.Format("\tSticks Left {0} {1} Right {2} {3}\n", state[i].ThumbSticks.Left.X, state[i].ThumbSticks.Left.Y, state[i].ThumbSticks.Right.X, state[i].ThumbSticks.Right.Y);
-		GUI.Label(new Rect(0 + i * 160, 0 + i * 160, Screen.width, Screen.height), text);
-		}
 
-	}
+	//void FixedUpdate()
+	//{
+	//	for (int i = 0; i < gamepadIndex.Count; ++i)
+	//	{
+	//		//SetVibration should be sent in a slower rate.
+	//		//Set vibration according to triggers
+	//		GamePad.SetVibration((PlayerIndex)i, state[i].Triggers.Left, state[i].Triggers.Right);
+	//	}
+	//}
+
+	//void OnGUI()
+	//{
+	//	for (int i = 0; i < gamepadIndex.Count; ++i)
+	//	{
+	//		string text = string.Format("Player {0} \n", i);
+	//		text += string.Format("IsConnected {0} Packet #{1}\n", state[i].IsConnected, state[i].PacketNumber);
+	//		text += string.Format("\tTriggers {0} {1}\n", state[i].Triggers.Left, state[i].Triggers.Right);
+	//		text += string.Format("\tD-Pad {0} {1} {2} {3}\n", state[i].DPad.Up, state[i].DPad.Right, state[i].DPad.Down, state[i].DPad.Left);
+	//		text += string.Format("\tButtons Start {0} Back {1} Guide {2}\n", state[i].Buttons.Start, state[i].Buttons.Back, state[i].Buttons.Guide);
+	//		text += string.Format("\tButtons LeftStick {0} RightStick {1} LeftShoulder {2} RightShoulder {3}\n", state[i].Buttons.LeftStick, state[i].Buttons.RightStick, state[i].Buttons.LeftShoulder, state[i].Buttons.RightShoulder);
+	//		text += string.Format("\tButtons A {0} B {1} X {2} Y {3}\n", state[i].Buttons.A, state[i].Buttons.B, state[i].Buttons.X, state[i].Buttons.Y);
+	//		text += string.Format("\tSticks Left {0} {1} Right {2} {3}\n", state[i].ThumbSticks.Left.X, state[i].ThumbSticks.Left.Y, state[i].ThumbSticks.Right.X, state[i].ThumbSticks.Right.Y);
+	//		GUI.Label(new Rect(0 + i * 160, 0, Screen.width, Screen.height), text);
+	//	}
+
+	//}
 }
--- ClashOfGreedyCrooks/Assets/_Scripts/PlayerController.cs
+++ ClashOfGreedyCrooks/Assets/_Scripts/PlayerController.cs
@@ -2,36 +2,85 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+	public float speed = 5f;
+	private float aimAngle = 0f;
+	private float dt = 0f;
 
-    public float speed;
-    public int playernumber;
-    private float moveHorizontal;
-    private float moveVertical;
+	private Vector3 movement;
+	private Vector3 directionalInputLeftStick;
+	private Vector3 directionalInputRightStick;
 
-    private Rigidbody rb;
 
-	// Use this for initialization
-	void Start () {
-        rb = GetComponent<Rigidbody>();
+	private void Start()
+	{
+		directionalInputLeftStick = Vector3.zero;
+		directionalInputRightStick = Vector3.zero;
+		movement = Vector3.zero;
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate ()
-    {
-        if (playernumber == 1)
-        {
-            moveHorizontal = Input.GetAxis("Horizontal");
-            moveVertical = Input.GetAxis("Vertical");
-        }else if (playernumber == 2)
-        {
-            moveHorizontal = Input.GetAxis("Horizontal2");
-            moveVertical = Input.GetAxis("Vertical2");
-        }
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+	private void Update()
+	{
+		dt = Time.deltaTime;
 
-        transform.position += movement * speed;
-    }
-    
+		CalculateMovement();
+		MovePlayer();
+	}
+
+	private void CalculateMovement()
+	{
+		movement = new Vector3(directionalInputLeftStick.x, 0f, directionalInputLeftStick.z);
+		aimAngle = Mathf.Atan2(directionalInputRightStick.x, directionalInputRightStick.z) * Mathf.Rad2Deg;
+	}
+
+	private void MovePlayer()
+	{
+		transform.position += movement * dt * speed;
+		transform.rotation = Quaternion.AngleAxis(aimAngle, Vector3.up);
+	}
+
+	public void Shoot()
+	{
+		Debug.Log("SHOOT");
+	}
+
+	public void SetDirectionalInput(Vector3 leftStick, Vector3 rightStick)
+	{
+		directionalInputLeftStick = leftStick;
+		directionalInputRightStick = rightStick;
+	}
+
+
+	//------------
+	//   public float speed;
+	//   public int playernumber;
+	//   private float moveHorizontal;
+	//   private float moveVertical;
+
+	//   private Rigidbody rb;
+
+	//// Use this for initialization
+	//void Start () {
+	//       rb = GetComponent<Rigidbody>();
+	//}
+
+	//// Update is called once per frame
+	//void FixedUpdate ()
+	//   {
+	//       if (playernumber == 1)
+	//       {
+	//           moveHorizontal = Input.GetAxis("Horizontal");
+	//           moveVertical = Input.GetAxis("Vertical");
+	//       }else if (playernumber == 2)
+	//       {
+	//           moveHorizontal = Input.GetAxis("Horizontal2");
+	//           moveVertical = Input.GetAxis("Vertical2");
+	//       }
+
+	//       Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+	//       transform.position += movement * speed;
+	//   }
+	//--------------- 
 }
