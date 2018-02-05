a7e037ad5fa4ec8a51e4f02e849b28eb74d109bd
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2018-02-05 16:14:58 +0100

    Added Wwise to project by editing the fucking source code

---
+++ ClashOfGreedyCrooks/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,52 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AMB_ARENA = 3854123299U;
+        static const AkUniqueID AMB_MENU = 2552542987U;
+        static const AkUniqueID AMB_PICKING = 2983184963U;
+        static const AkUniqueID BUTTONACCEPT = 69462689U;
+        static const AkUniqueID BUTTONDECLINE = 891695653U;
+        static const AkUniqueID BUTTONDISCONNECT = 2164585015U;
+        static const AkUniqueID BUTTONHOVER = 3035572085U;
+        static const AkUniqueID BUTTONJOIN = 2024546869U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID WWISESOUNDBANK = 1238300023U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID IMPACTS = 1316910230U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MISC = 3062523257U;
+        static const AkUniqueID MU = 1685527043U;
+        static const AkUniqueID PROJECTILES = 4275850663U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID COMMUNICATION = 530303819U;
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
