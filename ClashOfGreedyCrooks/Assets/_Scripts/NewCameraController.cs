c437f15917f35e441ad04a483e53b6899ca29d12
spirran <31646656+spirran@users.noreply.github.com>
2018-02-05 14:52:49 +0100

    Camera now works and zooms in X-led! I will continue trying making it zooming in Z-led.

--- ClashOfGreedyCrooks/Assets/_Scripts/NewCameraController.cs
+++ ClashOfGreedyCrooks/Assets/_Scripts/NewCameraController.cs
@@ -2,29 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class NewCameraController : MonoBehaviour
 {
     public List<Transform> targets;
 
     public Vector3 offset;
+    public float smoothTime = .5f;
+
+    public float minZoom = 40f;
+    public float maxZoom = 10f;
+    public float zoomLimiter = 50f;
 
     private Vector3 velocity;
+    private Camera cam;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
 
     private void LateUpdate()
     {
         if (targets.Count == 0)
             return;
 
+        Move();
+        Zoom();
+ 
+    }
+
+    void Move()
+    {
         Vector3 centerPoint = GetCenterPoint();
 
         Vector3 newPosition = centerPoint + offset;
 
-		//Commented out this line since it returned an error -fippan
-        //transform.position = Vector3.SmoothDamp(transform.position, newPosition)
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
     }
 
     Vector3 GetCenterPoint()
     {
+        //Same position as the only player left.
         if(targets.Count == 1)
         {
             return targets[0].position;
@@ -38,5 +57,22 @@ public class NewCameraController : MonoBehaviour
 
         return bounds.center;
     }
+
+    void Zoom()
+    {
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance() / zoomLimiter);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, newZoom, Time.deltaTime);
+    }
+
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+        return bounds.size.x;
+    }
+
 
 }
