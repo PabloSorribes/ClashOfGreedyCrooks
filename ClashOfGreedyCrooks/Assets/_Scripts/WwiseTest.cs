fa62960f5027f6fa3300e6cd4afb2b105f718923
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2018-02-06 10:20:45 +0100

    fixed wwise test script

--- ClashOfGreedyCrooks/Assets/_Scripts/WwiseTest.cs
+++ ClashOfGreedyCrooks/Assets/_Scripts/WwiseTest.cs
@@ -6,7 +6,7 @@ public class WwiseTest : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		AkSoundEngine.PostEvent("ButtonAccept", this.gameObject);
+		//AkSoundEngine.PostEvent("ButtonAccept", this.gameObject);
 	}
 	
 	// Update is called once per frame
