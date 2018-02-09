0671a4d24b2999196b1c70b8410bc45b6924dee8
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2018-02-09 09:54:48 +0100

    implemented shooting sound

--- ClashOfGreedyCrooks/Assets/_Scripts/Shooting.cs
+++ ClashOfGreedyCrooks/Assets/_Scripts/Shooting.cs
@@ -19,11 +19,20 @@ public class Shooting : MonoBehaviour {
 
     private GameObject bulletspawned;
 
+	FMODUnity.StudioEventEmitter a_shoot;
+
     //TODO: Input should come through the PlayerController / which button activates it should be derived from the PlayerController.
     private void Start()
     {
+		InitializeAudio();
         bulletSpawnPoint = transform.Find("Champion").GetChild(0).Find("WeaponHold").GetChild(0).Find("Muzzle");
     }
+
+	private void InitializeAudio() {
+		a_shoot = gameObject.AddComponent<FMODUnity.StudioEventEmitter>();
+		a_shoot.Event = "event:/Arena/playerShoot";
+		a_shoot.Preload = true;
+	}
 
 	//TODO: Remove this when real Keyboard support is added.
 	private void Update() {
@@ -35,6 +44,8 @@ public class Shooting : MonoBehaviour {
 	//Spawns the bullet
 	public void Shoot()
     {
+		a_shoot.Play();
+
         bulletspawned = Instantiate(proj.gameObject, bulletSpawnPoint.position, Quaternion.identity);
         bulletspawned.transform.rotation = bulletSpawnPoint.transform.rotation;
         bulletspawned.GetComponent<Projectile>().damage = damage;
