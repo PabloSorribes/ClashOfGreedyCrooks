634b9808a7fd84d222a4b63f1411688ac5405557
FilipJinstrand <filip@jinstrand.se>
2018-02-13 13:16:39 +0100

    Added a Back button in the player connect scene

---
+++ ClashOfGreedyCrooks/Assets/_Scripts/GoBackScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoBackScript : MonoBehaviour {
+
+    public Button backButton;
+
+	// Use this for initialization
+	void Start () {
+        Button btn = backButton.GetComponent<Button>();
+        btn.onClick.AddListener(GoBack);
+	}
+	
+	void GoBack()
+    {
+        GameStateManager.GetInstance.SetState(GameState.MainMenu);
+    }
+}
