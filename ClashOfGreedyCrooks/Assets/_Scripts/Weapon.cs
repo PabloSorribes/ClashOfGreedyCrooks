8e61d6878f0555dd5a005d63d5da63a858d57bd8
FilipJinstrand <filip@jinstrand.se>
2018-02-19 12:49:58 +0100

    Added spread in weapon script

--- ClashOfGreedyCrooks/Assets/_Scripts/Weapon.cs
+++ ClashOfGreedyCrooks/Assets/_Scripts/Weapon.cs
@@ -2,51 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour {
+public class Weapon : MonoBehaviour
+{
 
     public new string name;
-	public float damage = 20;
-	public float projectileSpeed = 20;
+    public float damage = 20;
+    public float projectileSpeed = 20;
 
-	//public string pathToModel = "Weapons/";
-	//public int damageMin;
-	//public int damageMax;
-	//public int attackSpeedMin;
-	//public int attackSpeedMax;
-	//public int range;
-	//public int projectileSpeed;
-	//public int projectileSpread;
+    [Header ("This is the min & max spread of projectiles")]
+    [Range(-15, 0)]
+    public int spreadMin;
+    [Range(0, 15)]
+    public int spreadMax;
 
-	public Projectile proj;
-	private Transform bulletSpawnPoint;
-	private GameObject bulletspawned;
+    public bool blindFolded;
+
+    //public string pathToModel = "Weapons/";
+    //public int damageMin;
+    //public int damageMax;
+    //public int attackSpeedMin;
+    //public int attackSpeedMax;
+    //public int range;
+    //public int projectileSpeed;
+    //public int projectileSpread;
+
+    public Projectile proj;
+    private Transform bulletSpawnPoint;
+    private GameObject bulletspawned;
 
 
-	public void Start()
-	{
+    public void Start()
+    {
         bulletSpawnPoint = transform.Find("Muzzle");
 
-		//TODO: Uncomment after nerf has been implemented fully
-		//PlayerTokens.GetInstance.Drunk += BlindfoldedShot;
-	}
+        //TODO: Uncomment after nerf has been implemented fully
+        //PlayerTokens.GetInstance.Drunk += BlindfoldedShot;
+    }
 
-	public void InstantiateBullet(GameObject player)
-	{
-		bulletspawned = Instantiate(proj.gameObject, bulletSpawnPoint.position, Quaternion.identity);
-		bulletspawned.transform.rotation = bulletSpawnPoint.transform.rotation;
-		bulletspawned.GetComponent<Projectile>().damage = damage;
-		bulletspawned.GetComponent<Projectile>().projectileSpeed = projectileSpeed;
-		bulletspawned.GetComponent<Projectile>().player = player;
-	}
+    public void InstantiateBullet(GameObject player)
+    {
+        bulletspawned = Instantiate(proj.gameObject, bulletSpawnPoint.position, bulletSpawnPoint.transform.rotation);
 
-	//A player token nerf
-	//TODO: Maked the shot shoot random inside of an angle
-	//public void BlindfoldedShot()
-	//{
-	//	bulletspawned = Instantiate(proj.gameObject, bulletSpawnPoint.position, Quaternion.identity);
-	//	bulletspawned.transform.rotation = bulletSpawnPoint.transform.rotation;
-	//	bulletspawned.GetComponent<Projectile>().damage = damage;
-	//	bulletspawned.GetComponent<Projectile>().projectileSpeed = projectileSpeed;
-	//	bulletspawned.GetComponent<Projectile>().player = gameObject;
-	//}
+        if (blindFolded)
+        {
+            bulletspawned.transform.Rotate(new Vector3(0.0f, Random.Range(spreadMin, spreadMax), 0.0f));
+        }
+
+
+        bulletspawned.GetComponent<Projectile>().damage = damage;
+        bulletspawned.GetComponent<Projectile>().projectileSpeed = projectileSpeed;
+        bulletspawned.GetComponent<Projectile>().player = player;
+    }
+
+    //A player token nerf
+    //TODO: Maked the shot shoot random inside of an angle
+    //public void BlindfoldedShot()
+    //{
+    //	bulletspawned = Instantiate(proj.gameObject, bulletSpawnPoint.position, Quaternion.identity);
+    //	bulletspawned.transform.rotation = bulletSpawnPoint.transform.rotation;
+    //	bulletspawned.GetComponent<Projectile>().damage = damage;
+    //	bulletspawned.GetComponent<Projectile>().projectileSpeed = projectileSpeed;
+    //	bulletspawned.GetComponent<Projectile>().player = gameObject;
+    //}
 }
