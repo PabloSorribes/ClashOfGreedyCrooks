ea1f9ea1c336013bedaee96638927590c43bb17d
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2018-02-22 17:35:29 +0100

    added rotation loop to buoy

--- ClashOfGreedyCrooks/Assets/_Scripts/RotationScript.cs
+++ ClashOfGreedyCrooks/Assets/_Scripts/RotationScript.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class RotationScript : MonoBehaviour {
 
+	FMODUnity.StudioEventEmitter a_buoyRotation;
+
+	private void Start()
+	{
+		a_buoyRotation = AudioManager.GetInstance.InitializeAudioOnObject(this.gameObject, "event:/Arena/buoyLoop");
+		a_buoyRotation.Play();
+	}
 
 	// Update is called once per frame
 	void Update () {
-		transform.eulerAngles = transform.eulerAngles += Vector3.up * 10;
+		transform.eulerAngles = transform.eulerAngles += Vector3.up * 20;
+	}
+
+	private void OnDestroy()
+	{
+		a_buoyRotation.Stop();
 	}
 }
