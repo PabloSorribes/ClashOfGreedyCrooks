18406b66bbbfe7b0b65b359577cb35f0a5f3ec73
FilipJinstrand <filip@jinstrand.se>
2018-02-22 14:12:06 +0100

    New particle for explosion

---
+++ ClashOfGreedyCrooks/Assets/_Scripts/LauncherBlast.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LauncherBlast : MonoBehaviour {
+
+    private float projectileSpeed;
+    private float damage;
+    private float bulletLifeTime;
+
+    private GameObject player;
+
+    public void ProjectileSetup(float damage, float projectileSpeed, float bulletLifeTime, GameObject player)
+    {
+        this.damage = damage;
+        this.projectileSpeed = projectileSpeed;
+        this.player = player;
+        this.bulletLifeTime = bulletLifeTime;
+
+        player.GetComponent<PlayerInfo>().totalShotsFired++;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player" && other.gameObject != player)
+        {
+            player.GetComponent<PlayerInfo>().totalHits++;
+            player.GetComponent<PlayerInfo>().totalDamage += damage;
+
+            other.gameObject.GetComponent<PlayerHealth>().TakeDamage(damage, player.GetComponent<PlayerInfo>());
+
+            Destroy(gameObject);
+        }
+    }
+}
